4cd32643f44dd048d5b4e51c724dc2b4442c5d86
shivparekh21 <shivparekh21@gmail.com>
2024-11-08 10:23:17 -0800

    Added a new component header.

--- frontend/youtube-clone-ui/src/app/app.component.ts
+++ frontend/youtube-clone-ui/src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgxFileDropModule } from 'ngx-file-drop';
+import {HeaderComponent} from './header/header.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgxFileDropModule],
+  imports: [RouterOutlet, FormsModule, NgxFileDropModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
