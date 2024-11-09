45187f138cbb7f84a304b5b29df134ceb59a048d
shivparekh21 <shivparekh21@gmail.com>
2024-11-08 20:29:19 -0800

    Install flex_layout and cdk to use it in save-video-details.component.html and also used form from angular material.

--- frontend/youtube-clone-ui/src/app/app.component.ts
+++ frontend/youtube-clone-ui/src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgxFileDropModule } from 'ngx-file-drop';
 import {HeaderComponent} from './header/header.component';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgxFileDropModule, HeaderComponent],
+  imports: [RouterOutlet, FormsModule, NgxFileDropModule, HeaderComponent, FlexLayoutModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatOption, MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {FlexLayoutServerModule} from '@angular/flex-layout/server';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-save-video-details',
   standalone: true,
-  imports: [],
+  imports: [
+    FlexLayoutServerModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOption,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButton
+  ],
   templateUrl: './save-video-details.component.html',
   styleUrl: './save-video-details.component.css'
 })
-export class SaveVideoDetailsComponent {
 
+
+export class SaveVideoDetailsComponent {
+  title: FormControl = new FormControl('');
+  description: FormControl = new FormControl('');
+  videoStatus: FormControl = new FormControl('');
+
+  // creating a form group and binding it to ../../component.html
+  saveVideoDetailsForm: FormGroup;
+  constructor() {
+    this.saveVideoDetailsForm = new FormGroup({
+      title: this.title,
+      description: this.description,
+      videoStatus: this.videoStatus,
+    })
+  }
 }
